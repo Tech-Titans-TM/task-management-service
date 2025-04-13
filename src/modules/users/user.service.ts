ab42e79c146f0297c53a044169e0f2e790afdcfe
AnuruddhaAbeysinghe <anuabeysinghe93@gmail.com>
2025-04-13 22:41:25 +0530

    implementation for delete user

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -1,6 +1,6 @@
 import HttpException from "../../util/http-exception.model";
 import { IUser } from "./models/user.model";
-import { updateUser } from "./user.repository";
+import { deleteUser, updateUser } from "./user.repository";
  
 export const updateUserDetails = async (id: string, updateUserData: IUser) => {
   try {
@@ -16,4 +16,34 @@ export const updateUserDetails = async (id: string, updateUserData: IUser) => {
     throw error;
   }
 };
+
+export const deleteUserById = async (id: string) => {
+  try {
+    const user = await getUserById(id);
+ 
+    if (user === null) {
+      throw new HttpException(202, {
+        message: `User ID : ${id} does not exist`,
+      });
+    }
+ 
+    const deletedUser = await deleteUser(id);
+    if (!deletedUser) {
+      throw new HttpException(500, {
+        message: `Error in deleting user ID: ${id}`,
+        result: false,
+      });
+    }
+ 
+    return {
+      message: `User successfully deleted: ID: ${deletedUser._id}, email: ${deletedUser.email}`,
+    };
+  } catch (error: any) {
+    throw error;
+  }
+};
+
+function getUserById(id: string) {
+  throw new Error("Function not implemented.");
+}
  
