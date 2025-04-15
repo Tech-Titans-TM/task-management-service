89aa27954629575cc8c4ac0dc82787e59385054f
shafeemhsn <82707683+shafeemhsn@users.noreply.github.com>
2025-04-15 11:07:04 +0530

    Create newrelic.js

---
+++ newrelic.js
@@ -0,0 +1,16 @@
+"use strict";
+
+exports.config = {
+  app_name: ["tick-flow-service"],
+  license_key: "25a67f18fc12e90dd605804a8f32c182FFFFNRAL",
+
+  logging: {
+    level: "info",
+  },
+
+  allow_all_headers: true,
+
+  attributes: {
+    enabled: true,
+  },
+};
